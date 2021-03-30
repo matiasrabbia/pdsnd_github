b88aec428a57500bbdc22887d78f30f56b68082e
Matías Rabbia <matias.rabbia@angloamerican.com>
2021-03-29 23:03:02 -0300

    Add coment to indicate declaration of usefull variables section

--- bikeshare.py
+++ bikeshare.py
@@ -3,6 +3,7 @@ import time
 import pandas as pd
 import numpy as np
 
+#Seting usefull dictionnary and arrays
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
